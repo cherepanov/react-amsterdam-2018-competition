81901b1d78b44962fc72a48ac3ae151802c2e361
Aleka <a@cherepanov.info>
2018-03-04 10:11:33 +0300

    font loader

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  custom: {
+    families: ['Open Sans', 'sans-serif'],
+    urls: ['/fonts/fonts.css']
+  },
+  google: {
+    families: ['Lato:100,100i,300,300i,400,400i,700,700i,900,900i&subset=latin-ext']
+  }
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
