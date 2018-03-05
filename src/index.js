4052be7dc8ac7d7915c18dc4d3ff5c222c588a4f
Aleka <a@cherepanov.info>
2018-03-05 13:08:39 +0300

    remove Lato font

--- src/index.js
+++ src/index.js
@@ -9,10 +9,9 @@ WebFont.load({
     families: ['Open Sans', 'sans-serif'],
     urls: ['/fonts/fonts.css']
   },
-  google: {
-    // families: ['Lato:100,100i,300,300i,400,400i,700,700i,900,900i&subset=latin-ext']
-    families: ['Lato:100,200,900&subset=latin-ext']
-  }
+  // google: {
+  //   families: ['Lato:100,200,900&subset=latin-ext']
+  // }
 });
 
 ReactDOM.render(<App />, document.getElementById('root'));
