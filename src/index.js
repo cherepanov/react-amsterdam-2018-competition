07d2ebd5d84b308f9d50ea14b87873b7d9a27563
Aleka <a@cherepanov.info>
2018-03-05 14:24:35 +0300

    test static generator

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import { render } from 'react-static-generator';
 import './index.css';
 import App from './App';
 import WebFont from 'webfontloader';
@@ -8,10 +9,8 @@ WebFont.load({
   custom: {
     families: ['Open Sans', 'sans-serif'],
     urls: ['/fonts/fonts.css']
-  },
-  // google: {
-  //   families: ['Lato:100,200,900&subset=latin-ext']
-  // }
+  }
 });
 
 ReactDOM.render(<App />, document.getElementById('root'));
+// render(<App/>, document.getElementById('root'));
