79b5df41b887cb07afdd40c4e46199ea277be92a
Aleka <a@cherepanov.info>
2018-03-08 09:39:06 +0300

    avoid font flicker

--- src/App.js
+++ src/App.js
@@ -1,31 +1,44 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import Header from './components/Header'
 import Body from './components/Body'
 import Footer from './components/Footer'
+import FontFaceObserver from 'fontfaceobserver'
+
+const loadFont = () => {
+  const element = document.createElement('style');
+  element.type = 'text/css';
+  element.appendChild(document.createTextNode(
+    '@font-face {\n' +
+    '  font-family: \'Open Sans\';\n' +
+    '  font-style: normal;\n' +
+    '  font-weight: bold;\n' +
+    '  src: local(\'Open Sans\'), url(\'/fonts/Open_Sans/subset-OpenSans-Bold.woff2\') format(\'woff2\'), url(\'/fonts/Open_Sans/subset-OpenSans-Bold.woff\') format(\'woff\');\n' +
+    '}\n' +
+    '\n' +
+    '@font-face {\n' +
+    '  font-family: \'Open Sans\';\n' +
+    '  font-style: normal;\n' +
+    '  font-weight: 300;\n' +
+    '  src: local(\'Open Sans\'), url(\'/fonts/Open_Sans/subset-OpenSans-Light.woff2\') format(\'woff2\'), url(\'/fonts/Open_Sans/subset-OpenSans-Light.woff\') format(\'woff\');\n' +
+    '}'));
+  document.getElementsByTagName('head')[0].appendChild(element);
+  Promise.all([ // eslint-disable-line
+    new FontFaceObserver('Open Sans', {
+      weight: 'bold'
+    }).load(),
+    new FontFaceObserver('Open Sans', {
+      weight: 300
+    }).load()
+  ]).then(function () {
+    document.documentElement.className += ' fonts-loaded';
+  });
+};
 
 class App extends Component {
   componentDidMount() {
     document.addEventListener('readystatechange', () => {
-      debugger
       if (document.readyState === 'complete') {
-        const element = document.createElement('style');
-        element.type = 'text/css';
-        element.appendChild(document.createTextNode('html body * {\n' +
-          '    font-family: \'Open Sans\' !important;\n' +
-          '  }' + '@font-face {\n' +
-          '  font-family: \'Open Sans\';\n' +
-          '  font-style: normal;\n' +
-          '  font-weight: bold;\n' +
-          '  src: local(\'Open Sans\'), url(\'/fonts/Open_Sans/subset-OpenSans-Bold.woff2\') format(\'woff2\'), url(\'/fonts/Open_Sans/subset-OpenSans-Bold.woff\') format(\'woff\');\n' +
-          '}\n' +
-          '\n' +
-          '@font-face {\n' +
-          '  font-family: \'Open Sans\';\n' +
-          '  font-style: normal;\n' +
-          '  font-weight: 300;\n' +
-          '  src: local(\'Open Sans\'), url(\'/fonts/Open_Sans/subset-OpenSans-Light.woff2\') format(\'woff2\'), url(\'/fonts/Open_Sans/subset-OpenSans-Light.woff\') format(\'woff\');\n' +
-          '}'));
-        document.getElementsByTagName('head')[0].appendChild(element);
+        loadFont()
       }
     });
   }
@@ -33,9 +46,9 @@ class App extends Component {
   render() {
     return (
       <div style={{textAlign: 'center'}}>
-        <Header />
-        <Body />
-        <Footer />
+        <Header/>
+        <Body/>
+        <Footer/>
       </div>
     )
   }
